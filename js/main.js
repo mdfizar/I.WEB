cb42ba1ce2c68ae7f4d85a3e0bde827aa96dcf6c
Md Mostafizar Rahman <136453073+mdfizar@users.noreply.github.com>
2023-11-11 09:54:26 +0600

    first upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// SWIPER 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    Delay: 1000,
+    disableOnInteraction:false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
